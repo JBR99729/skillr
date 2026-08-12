e7b2f194f712d3eea327cf1e1cfd00e4fce0debe
JBR99729 <jbhasker9@gmail.com>
2026-08-12 12:36:00 +1000

    Bust cache for professional Foundation UI

--- pwa-register.js
+++ pwa-register.js
@@ -10,7 +10,7 @@
 
   if (!document.querySelector('script[data-skillr-foundation-professional-ui]')) {
     const professionalUiScript = document.createElement("script");
-    professionalUiScript.src = "/assets/foundation-maths-professional-ui.js?v=1";
+    professionalUiScript.src = "/assets/foundation-maths-professional-ui.js?v=2";
     professionalUiScript.dataset.skillrFoundationProfessionalUi = "true";
     document.head.appendChild(professionalUiScript);
   }
