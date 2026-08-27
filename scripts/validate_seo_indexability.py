6db4ff6662084c8901d1439c2703423ce4b712ea
JBR99729 <jbhasker9@gmail.com>
2026-08-27 21:59:06 +1000

    Fix AC9MFM02 legacy noindex alias
    * Fix AC9MFM02 legacy alias target
    
    * Update AC9MFM02 legacy alias repair target
    
    * Update AC9MFM02 legacy alias validator target

--- scripts/validate_seo_indexability.py
+++ scripts/validate_seo_indexability.py
@@ -26,7 +26,7 @@ REQUIRED_ALIAS_REDIRECTS = {
     "/quiz/year-1/math/addition-substraction-daily/":
         "/quiz/year-1/daily-drills/math/addition-subtraction-to-20/",
     "/foundation/Maths/foundation-days-of-the-week-and-daily-routines.html":
-        "/foundation/maths/foundation-days-of-the-week-and-daily-routines.html",
+        "/foundation/maths/ac9mfm02-sequence-days-of-the-week-and-times-of-the-day/",
     "/foundation/Maths/foundation-data-collection-sorting-and-comparing-activities.html":
         "/foundation/maths/ac9mfst01-collect-sort-and-compare-data-represented-by-objects-and-images/",
     "/quiz/grade-k/math/AC9MFN01/":
